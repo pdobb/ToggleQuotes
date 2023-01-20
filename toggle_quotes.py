8c6ef415dd26ca7368086f56a83de18d8df6b9f4
Paul DobbinSchmaltz <p.dobbinschmaltz@icloud.com>
2023-01-20 13:11:47 -0600

    Reorganize code
    I believe this is the more idiomatic Python way to define a utility
    method that is only needed withing the scope of a class and that doesn't
    need access to state. Wasn't sure how to do this at first so I had
    moved the method up and out of the scope of the class definition to
    make it easily callable (without using `self.`, for example), but that
    just doesn't seem to be how Python works.

--- toggle_quotes.py
+++ toggle_quotes.py
@@ -1,9 +1,6 @@
 import sublime
 import sublime_plugin
 
-def _toggle_quotes(text):
-    return text.translate(str.maketrans("'\"", "\"'"))
-
 class ToggleQuotesCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         for region in self.view.sel():
@@ -13,7 +10,11 @@ class ToggleQuotesCommand(sublime_plugin.TextCommand):
             if not region.empty():
                 current_text = self.view.substr(region)
 
-                new_text = _toggle_quotes(current_text)
+                new_text = self._toggle_quotes(current_text)
 
                 if new_text != current_text:
                     self.view.replace(edit, region, new_text)
+
+    @staticmethod
+    def _toggle_quotes(text):
+        return text.translate(str.maketrans("'\"", "\"'"))
